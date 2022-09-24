458327b2caf5b7bf7232352c72f446e5a9e0f13b
Emmanuel Essien <ehmaessien@gmail.com>
2022-09-25 00:42:09 +0100

    Setup Github Context

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Home from './component/pages/Home';
-import About from './component/pages/About';
-import NotFound from './component/pages/NotFound';
-import Navbar from './component/layout/Navbar';
-import Footer from './component/layout/Footer';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./component/pages/Home";
+import About from "./component/pages/About";
+import NotFound from "./component/pages/NotFound";
+import Navbar from "./component/layout/Navbar";
+import Footer from "./component/layout/Footer";
+import { GithubProvider } from "./context/github/GithubContext.js";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar/>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/about' element={<About />}/>
-            <Route path='/notFound' element={<NotFound />}/>
-            <Route path='/*' element={<NotFound />}/>
-          </Routes>
-        </main>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notFound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
 
-        <Footer/>
-      </div>
-    </Router>
-    
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- src/component/users/UserResult.jsx
+++ src/component/users/UserResult.jsx
@@ -1,38 +1,28 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 function UserResult() {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const {users, loading, fetchUsers} = useContext(GithubContext)
 
     useEffect(() => {
         fetchUsers()
     }, [])
 
-    const fetchUsers = async () => {
-        const response = await fetch
-        (`${process.env.REACT_APP_GITHUB_URL}/users`)
-
-        const data = await response.json()
-
-        setUsers(data)
-        setLoading(false)
-    }
-
-    if(!loading){
+    if (!loading) {
         return (
-        <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-        {users.map((user) => (
-            <UserItem key={user.id} user={user}/>
-        ))}
-        </div>
-    );
-    } else{
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+                {users.map((user) => (
+                    <UserItem key={user.id} user={user} />
+                ))}
+            </div>
+        );
+    } else {
         return <Spinner />
     }
 
-  
+
 }
 
 export default UserResult;
